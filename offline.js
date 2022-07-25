d9c68e1a3e859458c9a849456ad4b15419c5b192
Foxel1 <109957175+Foxel1@users.noreply.github.com>
2022-07-25 13:28:43 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1658743488,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite4-sheet0.png",
+		"images/player-sheet0.png",
+		"images/weapon1-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/targetmouse-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/item_shield-sheet0.png",
+		"images/step-sheet0.png",
+		"images/barrier-sheet0.png",
+		"images/pistol1-sheet0.png",
+		"images/enemy1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
